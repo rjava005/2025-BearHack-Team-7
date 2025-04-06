d4c16ff34cc0fb28eb292ec24a8468a86a474f26
Jack <ninjacats1001@gmail.com>
2025-04-06 10:55:38 -0700

    add periods

--- Bearhack-2025/include/tasks.h
+++ Bearhack-2025/include/tasks.h
@@ -16,6 +16,10 @@ typedef struct _task{
 
 //TASK PERIODS
 const unsigned long SONAR_TASK_PERIOD = 1000;
+const unsigned long MINUTE_PERIOD = 60000;
+const unsigned long SETCLOCK_PERIOD = 100;
+const unsigned long TIMEWARN_PERIOD = 100;
+const unsigned long LEDCTRL_PERIOD = 200;
 
 const unsigned long GCD_PERIOD = 1;
 
@@ -177,31 +181,26 @@ int RightButtonTick(int state3) {
 
 
 int checkTimeWarn(int state2c){
-  checkClock(currTime);
-  switch(timeWarn){
-    case false: break;
-    case true:  PORTD = PORTD & (0x01 << 1); /*set light to red*/ break;
-  }
-  return 0;
+  checkClock(currTime);  return 0;
 }
 
 int LEDControlTick(int state4){
   switch (state4){
     case LEDoff: 
-      if (true/*not off*/){
+      if (clockOn){
         if (!timeWarn){state4 = GreenON;}
         else {state4 = RedON;}
       } 
       break;
     case GreenON:
-      if (true/*not off*/){
+      if (clockOn){
         if (timeWarn){state4 = RedON;break;}
         else if (strobe){state4 = LEDoff;break;}
       }
       else {state4 = LEDoff;}
       break;
     case RedON: 
-      if (true/*not off*/){
+      if (clockOn){
         if (!timeWarn){state4 = GreenON;break;}
         else if (strobe){state4 = LEDoff;break;}
       }

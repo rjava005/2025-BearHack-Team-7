b8d4510ff969f7d062865e2469382535960f0135
Jack <ninjacats1001@gmail.com>
2025-04-05 20:21:52 -0700

    venting rn

--- Bearhack-2025/include/tasks.h
+++ Bearhack-2025/include/tasks.h
@@ -29,6 +29,7 @@ int distance_in = 0;
 
 enum MinuteState {MinOFF, MinCNT} state2a;
 int currTime = 0;
+enum setClockState {clock1,clock2,clock3,clock4};
 
 int SonarTick(int state1) {
     switch (state1) {
@@ -58,4 +59,24 @@ int SonarTick(int state1) {
     }
 }
 
-int MinuteTick(int state2a){currTime++;}
+int MinuteTick(int state2a){
+  switch (state2a){
+    case MinOFF: return 0;
+    case MinCNT: currTime++; return 0;
+  }
+}
+
+int setClockTick(int state2b){
+  int potRead = 0; // replace with ADC reading
+  if (potRead <= 255){state2b = clock1;}
+  else if (potRead>255&&potRead<=511) {state2b = clock2;}
+  else if (potRead>511&&potRead<=767) {state2b = clock3;} 
+  else {state2b = clock4;}
+  
+  switch (state2b) {
+    case clock1: setClock(1);
+    case clock2: setClock(2);
+    case clock3: setClock(3);
+    case clock4: setClock(4);
+  }
+}
